ea66c51151a37692c92e5992bae99f879623c052
damianfel7 <49472570+damianfel7@users.noreply.github.com>
2020-12-12 15:40:46 +0100

    Add files via upload

---
+++ asceplius/backend/User/Repository/UserRepository.php
@@ -0,0 +1,26 @@
+<?php namespace App\User\Repository;
+
+
+use App\Database\Connector;
+use App\Database\Repository\Repository;
+use App\User\Entity\UserEntity;
+use PDO;
+
+class UserRepository extends Repository {
+
+    /**
+     * UserRepository constructor.
+     */
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function getEntityName() {
+        return "App\User\Entity\UserEntity";
+    }
+
+    protected function getTableName() {
+        return "user";
+    }
+}
+?>
