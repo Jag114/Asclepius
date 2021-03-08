791ac310b7a9e6f37a31e960973186ecdaccce96
damianfel7 <49472570+damianfel7@users.noreply.github.com>
2021-03-08 21:48:13 +0100

    wrzucansko w app

---
+++ asceplius/backend/App/User/Controller/UserController.php
@@ -0,0 +1,68 @@
+<?php
+	namespace App\User\Controller;
+
+	use App\Router\RestBodyReader;
+	use App\Serializer\JsonSerializer;
+	use App\User\Model\UserRequest;
+	use App\User\Service\UserService;
+
+	/**
+	 * @Controller(path="/user")
+	 */
+	class UserController {
+		/**
+		 * @var UserService
+		 */
+		private $userService;
+
+		/**
+		 * UserController constructor.
+		 */
+		public function __construct() {
+			$this->userService = new UserService();
+		}
+
+
+		/**
+		 * @Action(method="GET")
+		 */
+		public function getUsers() {
+			echo json_encode(array("test" => "hahaxd"));
+		}
+
+		/**
+		 * @Action(method="POST")
+		 */
+		public function addUser() {
+			/** @var UserRequest $requestBody */
+			$requestBody = RestBodyReader::readBody(UserRequest::class);
+
+			$user = $this->userService->addUser($requestBody);
+
+			echo JsonSerializer::getInstance()->serialize($user, 'json');
+		}
+
+		/**
+		 * @Action(method="GET", path="/{id}")
+		 */
+		public function getUser($id) {
+			$user = $this->userService->getUser($id);
+
+			echo JsonSerializer::getInstance()->serialize($user, 'json');
+		}
+
+		/**
+		 * @Action(method="PUT", path="/{id}")
+		 */
+		public function updateUser($id) {
+			echo sprintf("Updated user with id: %s", array($id));
+		}
+
+		/**
+		 * @Action(method="DELETE", path="/{id}")
+		 */
+		public function deleteUser($id) {
+			echo sprintf("Deleted user with id: %s", array($id));
+		}
+	}
+?>
---
+++ asceplius/backend/App/User/Service/AuthService.php
@@ -0,0 +1,29 @@
+<?php
+	namespace App\User\Service;
+
+	use App\Helpers\JwtHelper;
+	use App\User\Model\Token;
+
+	class AuthService {
+
+		private $userService;
+
+		/**
+		 * AuthService constructor.
+		 */
+		public function __construct() {
+			$this->userService = new UserService();
+		}
+
+		public function login($username, $password) {
+
+			$isUserExists = $this->userService->isUserWithPasswordExists($username, $password);
+
+			if ($isUserExists) {
+				return new Token(JwtHelper::generateUserToken($username));
+			}
+
+			return null;
+		}
+	}	
+?>
